82af30e8f8a5241cdd315c3d60c065b64a698e14
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-11-05 15:42:19 +0100

    Convert Item to JPA entity

--- src/main/java/io/archilab/fae/faespringdemo/item/Item.java
+++ src/main/java/io/archilab/fae/faespringdemo/item/Item.java
@@ -1,13 +1,21 @@
 package io.archilab.fae.faespringdemo.item;
 
 import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.Data;
 
+@Entity
 @Data
 public class Item {
 
+  @Id
   private UUID id;
   private String name;
+
+  protected Item() {
+    this.id = UUID.randomUUID();
+  }
 
   public Item(String name) {
     this.id = UUID.randomUUID();
