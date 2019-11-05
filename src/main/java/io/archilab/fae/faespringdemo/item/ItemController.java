048ec5892d153ab072740a34d02dc38708576214
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-11-05 15:43:01 +0100

    Use ItemRepository in ItemController

--- src/main/java/io/archilab/fae/faespringdemo/item/ItemController.java
+++ src/main/java/io/archilab/fae/faespringdemo/item/ItemController.java
@@ -1,14 +1,28 @@
 package io.archilab.fae.faespringdemo.item;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
 
-  @GetMapping("/item")
-  public Item getItem() {
-    return new Item("item1");
+  private final ItemRepository itemRepository;
+
+  public ItemController(ItemRepository itemRepository) {
+    this.itemRepository = itemRepository;
+  }
+
+  @GetMapping("/items")
+  public List<Item> getAllItems() {
+    return this.itemRepository.findAll();
+  }
+
+  @PostMapping("/items")
+  public Item postItem(@RequestBody Item item) {
+    return this.itemRepository.save(item);
   }
 
 }
